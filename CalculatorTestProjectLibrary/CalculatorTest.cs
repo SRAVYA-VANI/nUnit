13b97c09f1d536e0ea92e724a302e9a4310ec88f
sravy <sravy@DESKTOP-KDG18BJ>
2023-02-14 14:52:44 +0530

    Add project files.

---
+++ CalculatorTestProjectLibrary/CalculatorTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using UnitTestDemo;
+namespace CalculatorTestProjectLibrary
+{
+    [TestFixture]
+    public class CalculatorTest//: IDisposable
+    {
+        Calculator calc;
+        public void SetUp()
+        {
+            calc = new Calculator();
+        }
+        //[TearDown]
+        //public void TearDown()
+        //{
+        //    calc.Dispose();
+        //}
+
+
+        [Test,Order(3)]
+        
+        public void Test_Addition_with_valid_Integer()
+        {
+           var calc = new Calculator();
+            var res = calc.Addition(4, 3);
+            Assert.AreEqual(7, res);
+
+            var res1 = calc.Addition(4, 3);
+            Assert.AreEqual(7, res1);
+        }
+        [Test,Order(2)]
+        public void Test_subtraction_with_valid_negativeIntegers()
+        {
+           var calc = new Calculator();
+            var res = calc.subtraction(120, 70);
+            Assert.AreEqual(50, res);
+        }
+
+        [Test,Order(1)]
+        [TestCase(30,20,10)]
+        [TestCase(400,300,100)]
+        [TestCase(500,200,300)]
+        public void Test_Sub_with_multiple(int x, int y, int expectedres)
+        {
+           var calc = new Calculator();
+            var res = calc.subtraction(x, y);
+            Assert.AreEqual(expectedres, res);
+        }
+
+    }
+}
---
+++ nUnit/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UnitTestDemo
+
+{
+    public class Calculator
+    {
+        public int Addition(int x, int y)
+        {
+            return x + y;
+        }
+        public int subtraction(int x, int y)
+        {
+            if (x < y)
+            {
+                throw new ArgumentException($"{x} is less than{ y}");
+            }
+            return x - y;
+        }
+        public void Dispose()
+        {
+            throw new Exception("releasing memory");
+        }
+       
+    }
+}
